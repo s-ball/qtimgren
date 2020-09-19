b98eb78301e591799c19ce2a6275742f1680586e
s-ball <s-ball@laposte.net>
2020-09-19 13:24:23 +0200

    Extract setup configuration to a setup.cfg file.

--- setup.py
+++ setup.py
@@ -6,16 +6,20 @@ import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from setuptools import setup
+from warnings import warn
+
 try:
     from setuptools_scm import get_version as scm_version
 except ImportError:
-    def scm_version(*_args, **_kwargs): raise LookupError
+    def scm_version(*_args, **_kwargs):
+        raise LookupError
 
 import os.path
 import re
 import subprocess
 
 import sys
+
 sys.path.append('.')
 
 name = 'qtimgren'
@@ -25,7 +29,7 @@ wd = os.path.abspath(os.path.dirname(__file__))
 def get_version() -> str:
     """ extract version number """
     _version = '0.0.0'  # fallback value should never be used
-    try:    # first from git using setuptools_scm
+    try:  # first from git using setuptools_scm
         _version = scm_version(write_to=os.path.join(wd, name, 'version.py'))
     except LookupError:
         try:  # else from a previous version.py
@@ -33,9 +37,8 @@ def get_version() -> str:
                 for line in fd:
                     if line.startswith('version'):
                         _version = line.split("'")[1]
-        except OSError as e:
-            raise RuntimeError('Need either git+setuptools-scm or'
-                               ' version.py file') from e
+        except OSError:
+            warn('Need either git+setuptools-scm or version.py file')
     return _version
 
 
@@ -78,38 +81,7 @@ if __name__ == '__main__':
     version = get_version()
     long_description = get_long_desc(version)
     setup(
-        name=name,
         version=version,
-        packages=[name],
-        url='https://github.com/s-ball/QtImgren',
-        classifiers=[
-            # How mature is this project? Common values are
-            #   3 - Alpha
-            #   4 - Beta
-            #   5 - Production/Stable
-            'Development Status :: 3 - Alpha',
-
-            # Indicate who your project is intended for
-            'Intended Audience :: End Users/Desktop',
-            'Environment :: X11 Applications :: Qt',
-
-            # Pick your license as you wish (should match "license" above)
-            'License :: OSI Approved :: MIT License',
-
-            # Specify the Python versions you support here. In particular, ensure
-            # that you indicate whether you support Python 2, Python 3 or both.
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        python_requires='>=3.5',
-        install_requires=['pyimgren>=0.8',  'PySide2>=5.12'],
-        license='MIT',
-        author='SBA',
-        author_email='s-ball@laposte.net',
-        description='GUI over the pyimgren package',
         long_description=long_description,
         long_description_content_type='text/markdown',
         cmdclass={
@@ -117,5 +89,7 @@ if __name__ == '__main__':
             'build_qm': BuildQm,
             'build_rc': BuildRc,
             'build_py': build_py,
+            'build_ext': build_py,
+            'check': build_py,
         },
     )
